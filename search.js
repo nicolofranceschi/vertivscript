0f0ee3d30dc66164ec2a88707ecbd0f7b1bd8d26
Nicolofranceschi <franceschinicolo@gmail.com>
2024-04-22 21:32:49 +0200

    Update search.js to fix CSS styling and add font links

--- search.js
+++ search.js
@@ -23,12 +23,17 @@ function closeSearch() {
     document.getElementById("search").remove();
 }
 
+document.head.insertAdjacentHTML("afterend", `<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="https://nicolofranceschi.github.io/vertivscript/index.css">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">`);
+
 document.body.insertAdjacentHTML("afterend", `<div
 id="search"
 style='position:fixed; top:0%; left:0%; width:100%; height:100%; background-color:rgba(0,0,0,0.5); z-index:10000; display: flex; justify-content: center; align-items: center;'>
-<div  style="margin:50px;">
+<div style="margin:50px;">
     <div style="background-color:white; padding:20px; border-radius:10px; position: relative;">
-        <h1>Search</h1>
+        <h1 classname="inter-class" >Search</h1>
         <p style="position: absolute; top:10px; right:10px; cursor: pointer;" onclick="closeSearch()">X</p>
             <input id="searchValue" type="search" name="search" />
             <button type="submit" value="GO" onclick="searchElement(document.getElementById('searchValue').value)">GO</button>
