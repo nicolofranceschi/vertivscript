4e8aa9c6bdd2f9352915183a6614ec1ebf4afe64
Nicolofranceschi <franceschinicolo@gmail.com>
2024-04-22 22:58:32 +0200

    Refactor search.js to fix fetch URL, remove unused CSS and font links, and insert additional HTML content

--- search.js
+++ search.js
@@ -4,7 +4,7 @@ function searchElement(s) {
     for (let i = 0; i < x.length; i++) {
         if (x[i].label.toLowerCase().includes(s.toLowerCase())) filter.push({ value: x[i].value, label: x[i].label })
     }
-    console.log("filter ciao",filter)
+    console.log("filter ciao", filter)
     var form = document.getElementById("formPlace");
     for (let i = 0; i < filter.length; i++) {
         form.insertAdjacentHTML("beforeend", `<strong onclick="selectElement(${filter[i].value})">${filter[i].label}</strong>`);
@@ -22,20 +22,24 @@ function selectElement(value) {
 function closeSearch() {
     document.getElementById("searchUI").remove();
     document.getElementById("search").remove();
+    const ele = document.getElementsByName("add").remove();
+    for (var i = 0; i < ele.length; i++) {
+        ele[i].remove();
+    }
 }
 
-document.head.insertAdjacentHTML("beforeend", `<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link rel="stylesheet" href="https://vertivscript.vercel.app/plugin.css">
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">`);
+document.head.insertAdjacentHTML("beforeend", `<link name="add" rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect"  name="add" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet"  name="add" href="https://vertivscript.vercel.app/plugin.css">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"  name="add" rel="stylesheet">`);
 
 fetch('https://vertivscript.vercel.app/layer.html')
-    .then(function(response) {
+    .then(function (response) {
         return response.text()
     })
-    .then(function(html) {
-        document.body.insertAdjacentHTML("beforeend", html );
+    .then(function (html) {
+        document.body.insertAdjacentHTML("beforeend", html);
     })
-    .catch(function(err) {  
-        console.log('Failed to fetch page:', err);  
+    .catch(function (err) {
+        console.log('Failed to fetch page:', err);
     });
