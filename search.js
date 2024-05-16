568ece704ddfa8befe1262dabafd62a93c67cc86
nicolofranceschi <franceschinicolo@gmail.com>
2024-05-16 15:00:04 +0200

    Refactor search.js to update HTML content and fix fetch URL

--- search.js
+++ search.js
@@ -1,5 +1,7 @@
+var id = null;
+
 function searchElement(s) {
-    var x = document.getElementById("DocumentTemplateSelectionTemplateId").options;
+    var x = document.getElementById(id).options;
     var filter = [];
     for (let i = 0; i < x.length; i++) {
         if (x[i].label.toLowerCase().includes(s.toLowerCase())) filter.push({ value: x[i].value, label: x[i].label })
@@ -14,7 +16,7 @@ function searchElement(s) {
 
 function selectElement(value) {
     console.log(value)
-    let element = document.getElementById("DocumentTemplateSelectionTemplateId");
+    let element = document.getElementById(id);
     element.value = value;
     closeSearch();
 }
@@ -34,8 +36,9 @@ document.head.insertAdjacentHTML("beforeend", `<link id="fontinter" rel="preconn
 <link rel="stylesheet" id="cssfile" href="https://vertivscript.vercel.app/plugin.css">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"  id="interdata" rel="stylesheet">`);
 
-
-fetch('https://vertivscript.vercel.app/layer.html')
+document.addEventListener("click", (e) => {
+    id = e.target.id
+    fetch('https://vertivscript.vercel.app/layer.html')
     .then(function (response) {
         return response.text()
     })
@@ -45,3 +48,5 @@ fetch('https://vertivscript.vercel.app/layer.html')
     .catch(function (err) {
         console.log('Failed to fetch page:', err);
     });
+})
+
