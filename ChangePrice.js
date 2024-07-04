f6e1d3c2c979be6f381361dbb8812e9119c3df6f
nicolofranceschi <franceschinicolo@gmail.com>
2024-07-04 20:30:50 +0200

    Refactor ChangePrice.js to remove unnecessary string manipulation and improve code readability

--- ChangePrice.js
+++ ChangePrice.js
@@ -22,7 +22,7 @@ if(enteredValue){
 		setTimeout(function () {
 			console.log(parseFloat(item.value.replace(",",".")),parseFloat(enteredValue))
 			let elementFloatValue = parseFloat(item.value.replace(",",".")) / (1 - parseFloat(enteredValue)/100)		
-			let elementStringValue = elementFloatValue.toFixed(2).toString().replace(".",",")	
+			let elementStringValue = elementFloatValue.toFixed(2).toString()
 			
 			ShowMessage('Changing price ... ' + counter + "/" + elementList.length);
 			
